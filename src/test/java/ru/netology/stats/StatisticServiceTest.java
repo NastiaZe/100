1ceb1c561ce5d424d5279380b755c5a7014bdc88
NastiaZe <anastasiazemskova4212gmail.com>
2021-01-30 23:03:39 +0300

    test 100

--- src/test/java/ru/netology/stats/StatisticServiceTest.java
+++ src/test/java/ru/netology/stats/StatisticServiceTest.java
@@ -7,7 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatisticServiceTest {
 
     @Test
-    void findMax() {
+    void  findMaxAtFirstPosition(){
+        StatisticService service = new StatisticService();
+        long[] incomesInBillions = {12,5,8,4,5,3,8,6,11,11,12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void  findMaxNotAtFirstPosition() {
         StatisticService service = new StatisticService();
         long[] incomesInBillions = {10,15,9,2,7,8,7,16};
         long expected = 16;
